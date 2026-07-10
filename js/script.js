e2c4e066b71dda70c1506147c7c2eef52d0e9aba
Blaze <blaze_awa@163.com>
2026-07-10 19:35:09 +0800

    lookup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const data = [
   { group: '关于我们', items: [
     {title:'GitHub',url:'https://github.com/awatext',desc:'AwAText',tags:['代码','平台']},
-    {title:'Hack Chat',url:'https://hack.chat/?lounge',desc:'?lounge',tags:['问答','社区']}
+    {title:'Hack Chat',url:'https://hack.chat/?lounge',desc:'?lounge',tags:['问答','社区', 'HC']},
   ]},
   { group: '阿瓦兰系列', items: [
     {title:'阿瓦兰文集',url:'https://book.awaland.dpdns.org',desc:'记录 AwALand',tags:['文档']},
@@ -9,10 +9,13 @@ const data = [
     {title:'阿瓦兰图床',url:'https://pic.awaland.dpdns.org',desc:'永久图床',tags:['工具']},
     {title:'阿瓦兰剪贴板',url:'https://clip.awaland.dpdns.org',desc:'在线剪贴板',tags:['工具']},
   ]},
+  { group: 'HC系列', items: [
+    {title:'LOOKUP',url:'https://lookup.awaland.dpdns.org',desc:'查询历史记录',tags:['历史', '工具', 'HC']},
+    {title:'HC客户端',url:'https://hc.awaland.dpdns.org',desc:'部署hc++源码',tags:['工具', 'HC']},
+  ]},
   { group: '其他项目', items: [
-    {title:'HRF日期解码器',url:'https://hrf.awaland.dpdns.org',desc:'阿斯顿法国皇家快乐！',tags:['历史']},
+    {title:'HRF日期解码器',url:'https://hrf.awaland.dpdns.org',desc:'阿斯顿法国皇家快乐！',tags:['工具']},
     {title:'给DPG的留言',url:'https://dpg.awaland.dpdns.org',desc:'如题',tags:['历史']},
-    {title:'HC客户端',url:'https://hc.awaland.dpdns.org',desc:'部署hc++源码',tags:['工具']},
   ]}
 ];
 
